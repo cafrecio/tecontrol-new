e208d8b3d739d53c0e25c5b067621f786e6c2af5
Carlos Bonifacio <cafrecio@hotmail.com>
2023-05-02 07:03:45 -0300

    Productos con SideBar collapse

--- config/adminlte.php
+++ config/adminlte.php
@@ -154,6 +154,7 @@ return [
     'layout_fixed_navbar' => true,
     'layout_fixed_footer' => null,
     'layout_dark_mode' => false,
+    
 
     /*
     |--------------------------------------------------------------------------
@@ -211,7 +212,7 @@ return [
     */
 
     'sidebar_mini' => 'lg',
-    'sidebar_collapse' => false,
+    'sidebar_collapse' => true,
     'sidebar_collapse_auto_size' => false,
     'sidebar_collapse_remember' => false,
     'sidebar_collapse_remember_no_transition' => true,
